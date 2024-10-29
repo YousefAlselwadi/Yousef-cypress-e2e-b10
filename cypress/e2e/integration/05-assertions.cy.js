198583350c5317f5b735963ad97b5081717311f2
YousefAlselwadi <yalselwadi@gmail.com>
2024-10-29 15:21:59 -0500

    iFrame

--- cypress/e2e/integration/05-assertions.cy.js
+++ cypress/e2e/integration/05-assertions.cy.js
@@ -238,12 +238,10 @@ describe("Assertions", () => {
        * 4. Validate these elements are visible.
        */
   
-      const headings = [ 'Programming Languages', 'Automation Tools' ];
+      const headings = ["Programming Languages", "Automation Tools"];
   
       cy.get('[data-identifier="Headings"] > h4').each(($el, index) => {
-        cy.wrap($el)
-          .should("have.text", headings[index])
-          .and("be.visible")
+        cy.wrap($el).should("have.text", headings[index]).and("be.visible");
       });
   
       /**
@@ -252,5 +250,80 @@ describe("Assertions", () => {
        * 3. Validate their texts with expected text
        * 4. Validate checkboxes are visible, and enabled
        */
+  
+      const exp = ["Apple", "Microsoft", "Tesla"];
+  
+      cy.get("#checkbox-button-group div").each(($el, index) => {
+        cy.wrap($el).find("label").should("have.text", exp[index]);
+        cy.wrap($el).find("input").should("be.visible").and("be.enabled");
+      });
+    });
+  
+    it("Assertion Practices", () => {
+      /**
+       * 1. Go to https://techglobal-training.com/frontend
+       * 2. Navigate to 'Html Elements' card
+       *
+       * 3. From the "Text Inputs" section
+       * 4. Validate text input 1 and text input 2 is enabled
+       * 5. Validate text input 1 and text input 2 is not required
+       * 6. Enter your name and last name
+       */
+  
+      // cy.get('#text_input1').should('be.enabled');
+      // cy.get('#text_input2').should('be.enabled');
+  
+      // cy.get('#text_input1').should('not.have.attr', 'required');
+      // cy.get('#text_input2').should('not.have.attr', 'required');
+  
+      // cy.get('#text_input1').type('John');
+      // cy.get('#text_input2').type('Doe');
+  
+      const names = ["Tech", "Global"];
+  
+      cy.get('[id^="text_input"]').each(($el, index) => {
+        cy.wrap($el)
+          .should("be.enabled")
+          .type(names[index])
+          .and("not.have.attr", "required");
+      });
+  
+      /**
+       * 1. Go to https://techglobal-training.com/frontend
+       * 2. Navigate to 'Html Elements' card
+       *
+       * 3. From the "Date Inputs" section
+       * 4. Validate date input 1 and date input 2 is enabled
+       * 5. Validate date input 1 and date input 2 is is not required
+       * 6. Enter dates for both date input 1 and date input 2
+       * 7. Validate value is changed to given dates.
+       */
+      const dates = ["11/20/2022", "11/20/2021"];
+
+      cy.get('[id^="date_input"]').each(($el, index) => {
+        cy.wrap($el)
+          .clear()
+          .type(`${dates[index]}{enter}`)
+          .should("have.value", dates[index])
+          .and("be.enabled")
+          .and("not.have.attr", "required");
+      });
+          /**
+     * 1. Go to https://techglobal-training.com/frontend
+     * 2. Navigate to 'Html Elements' card
+     *
+     * 3. From the "Dropdowns" section
+     * 4. Validate dropdown 1 and dropdown 2 is enabled
+     * 6. Enter Microsoft for dropdown 1 and Apple for dropdown 2
+     * 7. Validate options are selected
+     */
+        const comp = ["Microsoft", "Apple"];
+
+        cy.get('[id^="company_dropdown"]').each(($el, index) => {
+        cy.wrap($el)
+        .should("be.enabled")
+        .select(comp[index])
+        .should('have.value', comp[index]);
+        });
     });
   });
