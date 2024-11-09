8103246919960b057e1442ecc5f1c31b29ddc6af
YousefAlselwadi <yalselwadi@gmail.com>
2024-11-09 11:12:32 -0600

    m

--- cypress.config.js
+++ cypress.config.js
@@ -9,10 +9,11 @@ export default defineConfig({
     UI_USERNAME: process.env.UI_USERNAME,
     UI_PASSWORD: process.env.UI_PASSWORD,
   },
-
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
     baseUrl: 'https://www.techglobal-training.com'
   },
