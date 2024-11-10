4b16c78892cc7ac908729c5200e203c95121b8fe
YousefAlselwadi <yalselwadi@gmail.com>
2024-11-10 17:22:14 -0600

    Project 03

--- cypress/e2e/projects/HmwProject03.cy.js
+++ cypress/e2e/projects/HmwProject03.cy.js
@@ -5,18 +5,51 @@ import BookingPage from "../../pages/BookingPage";
 const bookingPage = new BookingPage();
 
 describe("Book Your Trip From Tests", () => {
-    beforeEach(() => {
-        cy.visit('https://www.techglobal-training.com/frontend/project-3')
-    })
-    it("Test Case 01 - Validate the default Book your trip form", () => {
+  beforeEach(() => {
+    cy.visit("https://www.techglobal-training.com/frontend/project-3");
+  });
 
-        bookingPage.getOneWayRadioBtn().should("be.visible").and("be.enabled").and("be.checked");
-        bookingPage.getRoundTripRadioBtn().should("be.visible").and("be.enabled").and("not.be.checked");
-        bookingPage.getCabinClass().should("be.visible");
-        bookingPage.getFromClass().should("be.visible");
-        bookingPage.getToClass().should("be.visible");
-        bookingPage.getDepartClass().should("be.visible");
-        bookingPage.getReturnClass().should("be.visible").and("not.be.enabled");
-        bookingPage.getNumberClass().should("be.visible").and("contain", 7);
-    })
-})
+  const validateTextAndLabels = () => {
+    const text = [
+      "Trip type",
+      "Cabin Class",
+      "From",
+      "To",
+      "Depart",
+      "Return",
+      "Number of passengers",
+      "Passenger 1",
+    ];
+    bookingPage.getLabels().each(($el, index) => {
+      cy.wrap($el).should("be.visible").and("have.text", text[index]);
+    });
+  };
+
+  const validateDropDowns = () => {
+    bookingPage.getDropDowns().each(($el) => {
+      cy.wrap($el).should("be.visible");
+    });
+  };
+
+  it("Test Case 01-02 - Validate the default Book your trip form", () => {
+    bookingPage.getOneWay().should("be.visible").children().should("be.checked");
+    bookingPage.getRoundTrip().should("be.visible").children().should("not.be.checked");
+    bookingPage.getNumOfPassDropDown().should("have.value", 1);
+    bookingPage.getPassengerDropDown().should("have.value", "Adult (16-64)");
+    bookingPage.getDatePicker1().should("be.visible").and("be.enabled");
+    bookingPage.getDatePicker2().should("be.visible").and("be.disabled");
+    bookingPage.getBookButton().should("be.visible").and("be.enabled");
+    bookingPage.getRoundTrip().click()
+    bookingPage.getRadioButtonRoundTrip().should("be.checked")
+    bookingPage.getOneWayButton().should("not.be.checked")
+
+    validateTextAndLabels();
+    validateDropDowns();
+  });
+
+//   it("Test Case 02 - Validate the Book your trip form when Round trip is selected", () => {
+//     bookingPage.getOneWay().click();
+//     validateTextAndLabels();
+//     validateDropDowns();
+//   })
+});
--- cypress/pages/BookingPage.js
+++ cypress/pages/BookingPage.js
@@ -1,37 +1,51 @@
 class BookingPage {
-    getOneWayRadioBtn() {
-        return cy.get(".ml-0 > .mr-1")
+    getRadioButtons() {
+        return cy.get(".radio")
     }
 
-    getRoundTripRadioBtn() {
+    getOneWay() {
+        return this.getRadioButtons().eq(0)
+    }
+
+    getRoundTrip() {
+        return this.getRadioButtons().eq(1)
+    }
+
+    getLabels() {
+        return cy.get(".label")
+    }
+
+    getDropDowns() {
+        return cy.get(".select")
+    }
+
+    getNumOfPassDropDown() {
+        return cy.get(":nth-child(7) > .select > select")
+    }
+
+    getPassengerDropDown() {
+        return cy.get(":nth-child(8) > .select > select")
+    }
+
+    getBookButton() {
+        return cy.get(".Button_c_button__TmkRS");
+    }    
+
+    getDatePicker1() {
+        return cy.get('[placeholder="MM/DD/YY"]').first();
+    }
+
+    getDatePicker2() {
+        return cy.get('[placeholder="MM/DD/YY"]').eq(1);
+    }
+
+    getRadioButtonRoundTrip() {
         return cy.get(":nth-child(2) > .mr-1")
     }
 
-    getCabinClass() {
-        return cy.get(".field").eq(1);
+    getOneWayButton() {
+        return cy.get(".ml-0 > .mr-1")
     }
-    getFromClass() {
-        return cy.get(".field").eq(2);
-    }
-    getToClass() {
-        return cy.get(".field").eq(3);
-    }
-    getDepartClass() {
-        return cy.get(".field").eq(4);
-    }
-    getReturnClass() {
-        return cy.get(".field").eq(5);
-    }
-    getNumberClass() {
-        return cy.get(".field").eq(6);
-    }
-    getPassengerClass() {
-        return cy.get(".field").eq(7);
-    }
-    getBookClass() {
-        return cy.get(".field").eq(8);
-    }
-
 }
 
 export default BookingPage;
